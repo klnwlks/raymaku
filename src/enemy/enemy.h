fa5d27ce30877e880f27e6550ebce0e05feab07b
klnwlks <kalinawlukasaombebis@gmail.com>
2026-03-15 19:41:52 +0800

    feat: define enemy structure and pooling lifecycle functions

--- src/enemy/enemy.h
+++ src/enemy/enemy.h
@@ -2,12 +2,23 @@
 #define ENEMY_H
 
 #include "raylib.h"
+#include "../pattern/pattern.h"
 
 typedef struct {
     Vector2 position;
     Vector2 speed;
     int health;
-    // insert pattern here;
+    PatternConfig config;
+    Vector2 endPosition;
+    int appearanceLength;
+    float shootTimer; // delay between shots 
+    // TODO: add support for loading textures
 } Enemy;
+
+void InitEnemyPool(void);
+void UpdateEnemyPool(void);
+void DrawEnemyPool(void);
+void SpawnEnemy(Vector2 pos, Vector2 vel, int health, PatternConfig pattern,
+                Vector2 endpos, int appear, float shootTimer);
 
 #endif
